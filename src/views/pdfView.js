1161579d967342c404f2c35b291512b7be874804
Thomas-VWD <thomas.verwaerde@gmail.com>
2024-01-08 19:45:01 +0100

    wip

---
+++ src/views/pdfView.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const PDFDocument = require("pdfkit");
+const Mission = require("../controllers/missionController");
+
+const pdfView = {
+  generatePDF: async ({ filename = "output.pdf" }) => {
+    const doc = new PDFDocument();
+    const stream = fs.createWriteStream(filename);
+    doc.pipe(stream);
+
+    try {
+      const [missions] = await Mission.getMissions();
+      missions.forEach((mission, id) => {
+        if (id !== 0) {
+          doc.addPage();
+        }
+
+        doc
+          .fontSize(24)
+          .text(mission.titre, { align: "center", underline: true })
+          .text(mission.titre, { align: "center", underline: true });
+
+        const imageWidth = 450;
+        const imageHeight = 550;
+        const imageX = (doc.page.width - imageWidth) * 0.5;
+        const imageY = 120;
+
+        doc.image(mission.link, imageX, imageY, {
+          fit: [imageWidth, imageHeight],
+          align: "center",
+        });
+
+        doc
+          .text(mission.title, 100, 400)
+          .font("Times-Roman", 13)
+          .moveDown()
+          .text(mission.article, {
+            width: 412,
+            align: "justify",
+            indent: 30,
+            columns: 2,
+            height: 300,
+            ellipsis: true,
+          });
+      });
+
+      doc.end();
+      stream.on("finish", () => {
+        console.log("PDF généré avec succès !");
+      });
+    } catch (error) {
+      console.error("Erreur lors de la récupération des missions :", error);
+    }
+  },
+};
+
+module.exports = pdfView;
